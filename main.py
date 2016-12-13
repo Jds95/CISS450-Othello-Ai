029140daa06fa403fe7c1912b88c8bfb4d09756c
Jesse Simpson <jessedsimpson@hotmail.com>
2016-12-12 18:05:34 -0600

    Alpha beta pruning done, Mikey, fix that Evaluation function with your fancy stuff onegai

--- main.py
+++ main.py
@@ -6,100 +6,183 @@
 # ------------------------------------------
 # File: main.py
 # ------------------------------------------
+import os, copy
+# 8 directions
+dirx = [-1, 0, 1, -1, 1, -1, 0, 1]
+diry = [-1, -1, -1, 0, 0, 1, 1, 1]
 
-def out_of_range_check(x, y, n):
-	if x < 0 or y < 0 or x >= n or y >= n:
-		return True
-	else:
-		return False
+# Starting min value
+minEvalBoard = -1 
 
-def valid_move_check(board, piece, xs, ys, n):
-	# Place piece to test
-	board[xs][ys] = piece
-	if piece == 'B':
-		other_piece = 'W'
-	else:
-		other_piece = 'B'
+# Starting max value
+maxEvalBoard = 1000
 
-	# List to hold pieces to flip
-	flip = []
-	# Loop to Check NSEW Diagonals
-	for xdir, ydir in [[0,1], [1,1], [1,0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
-		x, y = xs, ys
-		x += xdir
-		y += ydir
-		# If [x][y] are out of scope, test next direciton
-		if out_of_range_check(x, y, n):
-			continue
-		# Goes into while loop if piece besides it is opposite piece
-		while board[x][y] == other_piece:
-			x += xdir
-			y += ydir
-			# If it leaves scope, break
-			if out_of_range_check(x, y, n):
-				break
+def InitBoard(size):
+    if size % 2 == 0: # if board size is even
+        z = (size - 2) / 2
+        board[z][z] = 'B'
+        board[size - 1 - z][z] = 'W'        
+        board[z][size - 1 - z] = 'W'
+        board[size - 1 - z][size - 1 - z] = 'B'
+        
+def PrintBoard():
+    m = len(str(8 - 1))
+    for y in range(8):
+        row = ''
+        for x in range(8):
+            row += board[y][x] 
+            row += ' ' * m
+        print row + ' ' + str(y)
+    print
+    row = ''
+    for x in range(8):
+        row += str(x).zfill(m) + ' '
+    print row + '\n'
 
-		if out_of_range_check(x, y, n):
-			continue
-		# This point we know we have valid move, back track pieces to swap
-		if board[x][y] == piece:
-			while True:
-				x -= xdir
-				y -= ydir
-				if x == xs and y == ys:
-					break
-				# Add to list of pieces to be swapped
-				flip.append([x,y])
+def MakeMove(board, size, x, y, player): 
+    total = 0
+    board[y][x] = player
+    for d in range(8):
+        ctr = 0
+        for i in range(size):
+            dx = x + dirx[d] * (i + 1)
+            dy = y + diry[d] * (i + 1)
+            if dx < 0 or dx > size - 1 or dy < 0 or dy > size - 1:
+                ctr = 0; break
+            elif board[dy][dx] == player:
+                break
+            elif board[dy][dx] == ' ':
+                ctr = 0; break
+            else:
+                ctr += 1
+        for i in range(ctr):
+            dx = x + dirx[d] * (i + 1)
+            dy = y + diry[d] * (i + 1)
+            board[dy][dx] = player
+        total += ctr
+    return (board, total)
 
-	#Put Space back onto board for [x][y] we checked
-	board[xs][ys] = ' '
+def ValidMove(board, size, x, y, player):
+    # Check for Invalid move less than 0 or greater than our index
+    if x < 0 or x > size - 1 or y < 0 or y > size - 1:
+        return False
+    if board[y][x] != ' ':
+        return False
 
-	#If no pieces to flip, invalid move
-	if len(flip) == 0:
-		return (False, flip)
+    (boardTemp, total) = MakeMove(copy.deepcopy(board), size, x, y, player)
+    if total == 0:
+        return False
+    return True
 
-	# Return True, list of x, y coordinates to flip
-	return (True, flip)
+def EvalBoard(board, size, player):
+    tot = 0
+    for y in range(size):
+        for x in range(size):
+            if board[y][x] == player:
+                # Corner check
+                if (x == 0 or x == size - 1) and (y == 0 or y == size - 1):
+                    tot += 4
+                # Side Check
+                elif (x == 0 or x == size - 1) or (y == 0 or y == size - 1):
+                    tot += 2
+                else:
+                    tot += 1
+    return tot
+
+# Test to see if part of tree we are at is out of moves
+def noMoveCheck(board, size, player):
+    for y in range(size):
+        for x in range(size):
+            if ValidMove(board, size, x, y, player):
+                return False
+    return True
+
+def AlphaBeta(board, size, player, depth, alpha, beta, maximizingPlayer):
+
+    if depth == 0 or noMoveCheck(board, size, player):
+        return EvalBoard(board, size, player)
+    if maximizingPlayer:
+        v = minEvalBoard
+        for y in range(size):
+            for x in range(size):
+                if ValidMove(board, size, x, y, player):
+                    (boardTemp, totctr) = MakeMove(copy.deepcopy(board), size, x, y, player)
+                    v = max(v, AlphaBeta(boardTemp, size, player, depth - 1, alpha, beta, False))
+                    alpha = max(alpha, v)
+                    if beta <= alpha:
+                        break # beta cut-off
+        return v
+    else: # minimizingPlayer
+        v = maxEvalBoard
+        for y in range(size):
+            for x in range(size):
+                if ValidMove(board, size, x, y, player):
+                    (boardTemp, totctr) = MakeMove(copy.deepcopy(board), size, x, y, player)
+                    v = min(v, AlphaBeta(boardTemp, size, player, depth - 1, alpha, beta, True))
+                    beta = min(beta, v)
+                    if beta <= alpha:
+                        break # alpha cut-off
+        return v
 
 
-def get_moves(board, piece, n):
-	moves = {}
-	for i in range(n):
-		for l in range(n):
-			if board[i][l] != ' ':
-				continue
-			y = valid_move_check(board, piece, i, l, n)
-			if y[0] == True:
-				# Append to moves the [x,y] of move and y[1] which is list of all pieces to flip.
-				moves[(i, l)] = y[1]
-	print moves
+def BestMove(board, size, player, depth):
+    # Starting min value
+    global minEvalBoard 
+    minEvalBoard= -1 
 
-def p_board(board, n):
-	s = "+-" * n + "+\n"
-	for item in board:
-		s += "|"
-		for i in item:
-			s += ''.join(i) + "|"
-		s += "\n" + "+-" * n + "+\n"
+    # Starting max value
+    global maxEvalBoard 
+    maxEvalBoard = size * size + 4 * size + 4 + 1 
 
-	print s
+    maxPoints = 0
+    mx = -1; my = -1
+    for y in range(size):
+        for x in range(size):
+            if ValidMove(board, size, x, y, player):
+                #(boardTemp, totctr) = MakeMove(copy.deepcopy(board), size, x, y, player)
+                points = AlphaBeta(board, size, player, depth, minEvalBoard, maxEvalBoard, True)
+                if points > maxPoints:
+                    maxPoints = points
+                    mx = x; my = y
+    return [my, mx]
 
-def initialize_board(board, n):
-	board[(n/2) - 1][(n/2)-1] = "B"
-	board[(n/2)][(n/2)] = "B"
-	board[(n/2) - 1][(n/2)] = "W"
-	board[(n/2)][(n/2-1)] = "W"
-	return board
+def swap_turn(turn):
+    if turn == 'B':
+        return 'W'
+    else:
+        return 'B'
 
-def main():
-	n = input()
-	x = []
-	for i in range(n):
-		y = [' '] * n
-		x.append(y)
-	y = initialize_board(x, n)
-	p_board(y, n)
-	get_moves(y, 'B', n)
+def get_move(board_size, board_state, turn, time_left, opponent_time_left):
 
-if __name__ == "__main__":
-    main()
+    if time_left > 100000:
+        move = BestMove(board_state, board_size, turn, 4)
+    else:
+        move = BestMove(board_state, board_size, turn, 2)        
+
+    if move != [-1, -1]:
+        return move
+    else:
+        return None
+
+
+# bsize = input()
+# board = [[' ' for x in range(bsize)] for y in range(bsize)]
+
+# InitBoard(bsize)
+# while True:
+#     print
+#     PrintBoard()
+#     turn = 'B'        
+#     (x, y) = get_move(bsize, board, turn, 1000000, 1000000)
+#     if not (x == -1 and y == -1):
+#             (board, totctr) = MakeMove(board, bsize, x, y, turn)
+#             print 'User played (X Y): ' + str(x) + ' ' + str(y)
+#             print '# of pieces taken: ' + str(totctr)
+#     turn = swap_turn(turn)
+#     print
+#     PrintBoard()
+#     (x, y) = get_move(bsize, board, turn, 1000000, 1000000)
+#     if not(x == -1 and y == -1):
+#         (board, totctr) = MakeMove(board, bsize, x, y, turn)
+#         print 'AI played (X Y): ' + str(x) + ' ' + str(y)
+#         print '# of pieces taken: ' + str(totctr)
